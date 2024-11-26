53c7bd15649a00bcfdf5b15b8a518d7d23a5e11a
haeun1107 <haeun9394@gmail.com>
2024-11-26 21:08:06 +0900

    [Fix] #27 - Cors 도메인 추가

--- src/main/java/dgu/sw/global/config/WebConfig.java
+++ src/main/java/dgu/sw/global/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080", "http://localhost:3000", "http://localhost:8081")
+                        .allowedOrigins("http://localhost:8080", "http://localhost:3000", "http://localhost:8081", "https://api.22s.store")
                         .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
