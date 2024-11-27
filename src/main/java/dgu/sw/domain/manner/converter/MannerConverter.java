267386a20b6d59c6741d61fe567c6756bc6a301f
dudtlstm <annpys1024@naver.com>
2024-11-28 02:29:46 +0900

    [Delete] #12 - 매너 설명서 검색 기능 & 즐겨찾기 기능 API 구현

---
+++ src/main/java/dgu/sw/domain/manner/converter/MannerConverter.java
@@ -0,0 +1,43 @@
+package dgu.sw.domain.manner.converter;
+
+import dgu.sw.domain.manner.dto.MannerDTO.MannerResponse.MannerDetailResponse;
+import dgu.sw.domain.manner.dto.MannerDTO.MannerResponse.MannerListResponse;
+import dgu.sw.domain.manner.dto.MannerDTO.MannerResponse.MannerFavoriteResponse;
+import dgu.sw.domain.manner.entity.Manner;
+
+public class MannerConverter {
+    public static MannerListResponse toMannerListResponse(Manner manner) {
+        return MannerListResponse.builder()
+                .mannerId(manner.getMannerId())
+                .category(manner.getCategory())
+                .title(manner.getTitle())
+                .contentPreview(manner.getContent().length() > 20 ? manner.getContent().substring(0, 20) + "..." : manner.getContent())
+                .imageUrl(manner.getImageUrl())
+                .build();
+    }
+
+    public static MannerDetailResponse toMannerDetailResponse(Manner manner) {
+        return MannerDetailResponse.builder()
+                .mannerId(manner.getMannerId())
+                .category(manner.getCategory())
+                .title(manner.getTitle())
+                .content(manner.getContent())
+                .imageUrl(manner.getImageUrl())
+                .build();
+    }
+
+    public static MannerFavoriteResponse toMannerFavoriteResponse(Manner manner) {
+        return MannerFavoriteResponse.builder()
+                .mannerId(manner.getMannerId())
+                .title(manner.getTitle())
+                .category(manner.getCategory())
+                .contentPreview(
+                        manner.getContent().length() > 20
+                                ? manner.getContent().substring(0, 20) + "..."
+                                : manner.getContent()
+                ) // 내용 20자 제한
+                .imageUrl(manner.getImageUrl())
+                .build();
+
+    }
+}
