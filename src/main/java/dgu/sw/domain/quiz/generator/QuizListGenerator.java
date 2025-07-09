bf313a341845ff068de18a7cd365ce7a86819c4c
한승우 <peter001019@naver.com>
2025-07-09 21:48:17 +0900

    [Feature] #189 MockTestGenerator 구현과 전략 패턴 적용

---
+++ src/main/java/dgu/sw/domain/quiz/generator/QuizListGenerator.java
@@ -0,0 +1,9 @@
+package dgu.sw.domain.quiz.generator;
+
+import dgu.sw.domain.quiz.entity.Quiz;
+
+import java.util.List;
+
+public interface QuizListGenerator {
+    public List<Quiz> generateQuizList();
+}
